1d7ed359b7a466acd90fb571b66a3638baadf389
Shayan H Poorian <shayanhpn@gmail.com>
2025-03-14 13:23:59 +0330

    add controller of registration user

---
+++ app/Http/Controllers/Admin/RegisterUserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterUserController extends Controller
+{
+    public function showRegisterForm(){
+        return view('user.register-user');
+    }
+
+
+    public function registerUser(Request $request){
+        $data = $request->validate([
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+        User::create($data);
+        return 'Done!!';
+
+    }
+}
