4eee8fafaca5d652f3ace8a0b90b9bb808c83707
Shayan H Poorian <shayanhpn@gmail.com>
2025-03-14 13:18:15 +0330

    add register-user get and post route

--- resources/views/dashboard/main-page.blade.php
+++
@@ -1,3 +0,0 @@
-<x-dash-layout>
-
-</x-dash-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Admin\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog/main-page');
 });
+
+Route::get('/dash',function(){
+    return view('dashboard/index');
+});
+
+Route::get('/register',[RegisterUserController::class,'showRegisterForm']);
+Route::post('/register',[RegisterUserController::class,'registerUser'])->name('register-user');
