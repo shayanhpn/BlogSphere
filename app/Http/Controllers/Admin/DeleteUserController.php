b6b048d067aaaee563a6d557b440aa2354378f8c
Shayan HPN <shayanpn@gmail.com>
2025-04-06 13:23:19 +0330

    added delete user functions

---
+++ app/Http/Controllers/Admin/DeleteUserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class DeleteUserController extends Controller
+{
+
+    public function showDeleteUserForm(User $user){
+        return view('user.delete-user',compact('user'));
+    }
+    public function deleteUser(User $user){
+        $user->delete();
+        return 'DELETED!!';
+    }
+}
