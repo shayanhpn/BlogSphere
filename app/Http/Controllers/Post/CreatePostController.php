c46931968af55bde9d37f8dfa83c3d58a8cfee81
Shayan HPN <shayanpn@gmail.com>
2025-04-22 22:34:08 +0330

    implement create post controller

--- app/Http/Controllers/Post/CreatePostController.php
+++ app/Http/Controllers/Post/CreatePostController.php
@@ -15,15 +15,36 @@ class CreatePostController extends Controller
     public function createPost(Request $request){
         $post_fields = $request->validate([
             'title' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'post_image' => 'nullable|image|mimes:png,jpg,webp|max:4096'
         ]);
 
         $post_fields['title'] = strip_tags($post_fields['title']);
         $post_fields['body'] = strip_tags($post_fields['body']);
         $post_fields['user_id'] = auth()->user()->id;
 
-        $result = Post::create($post_fields);
-        return 'Post Created!!!!';
+        $post_fields['post_image'] = null;
+
+        if($request->has('post_image')){
+            $image = $request->file('post_image');
+
+            $uploadPath = 'uploads/images';
+
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+
+            $image->move(public_path($uploadPath), $imageName);
+
+            $post_fields['post_image'] = $uploadPath . '/' . $imageName;
+
+        }
+
+        $result = Post::create([
+            'title' => $post_fields['title'],
+            'body' => $post_fields['body'],
+            'post_image' => $post_fields['post_image'],
+            'user_id' => $post_fields['user_id']
+        ]);
+        return redirect('/posts')->with('success','Post Has Been Created!');
 
        
     }
