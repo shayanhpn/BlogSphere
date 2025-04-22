9888a4dbef1f614722e307a1bc88e7faf84cb704
Shayan HPN <shayanpn@gmail.com>
2025-04-22 22:36:16 +0330

    add post_image column

--- database/migrations/2025_04_14_125554_create_posts.php
+++ database/migrations/2025_04_14_125554_create_posts.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->longText('body');
+            $table->string('post_image');
             $table->string('image')->nullable();
             $table->timestamps();
         });
